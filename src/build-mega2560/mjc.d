da96c8aff4fce8b77bb9e06ab0b7cbb852294ad4
SneManden <casper@snemanden.com>
2014-03-30 12:44:32 +0200

    Cloned to arduino-mk instead..

---
+++ src/build-mega2560/mjc.d
@@ -0,0 +1,12 @@
+build-mega2560/mjc.o: mjc.ino \
+ /usr/share/arduino//hardware/arduino/cores/arduino/Arduino.h \
+ /usr/share/arduino//hardware/arduino/cores/arduino/binary.h \
+ /usr/share/arduino//hardware/arduino/cores/arduino/WCharacter.h \
+ /usr/share/arduino//hardware/arduino/cores/arduino/WString.h \
+ /usr/share/arduino//hardware/arduino/cores/arduino/HardwareSerial.h \
+ /usr/share/arduino//hardware/arduino/cores/arduino/Stream.h \
+ /usr/share/arduino//hardware/arduino/cores/arduino/Print.h \
+ /usr/share/arduino//hardware/arduino/cores/arduino/Printable.h \
+ /usr/share/arduino//hardware/arduino/cores/arduino/new.h \
+ /usr/share/arduino//hardware/arduino/cores/arduino/pins_arduino.h \
+ lib/IRSensor/IRSensor.h
---
+++ src/build-mega2560/wiring_analog.d
@@ -0,0 +1,6 @@
+build-mega2560/wiring_analog.o: \
+ /usr/share/arduino//hardware/arduino/cores/arduino/wiring_analog.c \
+ /usr/share/arduino//hardware/arduino/cores/arduino/wiring_private.h \
+ /usr/share/arduino//hardware/arduino/cores/arduino/Arduino.h \
+ /usr/share/arduino//hardware/arduino/cores/arduino/binary.h \
+ /usr/share/arduino//hardware/arduino/cores/arduino/pins_arduino.h
